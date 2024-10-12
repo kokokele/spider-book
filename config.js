7e280f1c88794c549401c51732c02da176bc37f4
zhangpeng64 <zhangpeng64@meituan.com>
2024-10-12 10:09:01 +0800

    change congfig

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 module.exports = {
-	domain: 'https://www.feibzw.com/Html/39522/',
-	indexURL :'https://www.feibzw.com/Html/39522/index.html', // 
+	domain: 'https://www.feibzw.com/Html/51778/',
+	indexURL :'https://www.feibzw.com/Html/51778/index.html', 
 
-	bookName: 'xinglong', 
+	bookName: '四公子',
 	bookAuthor : 'x',
 	chapterAuthor: '-',
+	startChapter: 1,  //开始下载的章节，默认为1
 	// 封面图片
 	cover: '', 
 	// 并发数
@@ -15,3 +16,5 @@ module.exports = {
 'https://www.biduo.cc/biquge/6_6465/', // 万古神帝',
 'https://www.biduo.cc/biquge/43_43949/', // 生死统计,
 */
+// https://www.feibzw.com/Html/590/index.html  非常秘书
+
--- index2.js
+++ index2.js
@@ -1,6 +1,7 @@
 // 基于 https://m.lwxs.com/shu/63167/
 // https://www.dgzcgs.com/book/99623/
 const fs = require('fs');
+// https://www.npmjs.com/package/fetch 
 const fetch = require('fetch').fetchUrl;
 const cheerio = require('cheerio');
 const QueuePromise = require('promise-queue-easy');
@@ -20,117 +21,123 @@ const INDEX_URL = config.indexURL;
 
 const opt = {
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4240.75 Safari/537.36'
+        // 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4240.75 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+        // 'Sec-Ch-Ua-Mobile:': '?0',
+        // 'Sec-Ch-Ua-Platform:': '"macOS"',
+        // 'Upgrade-Insecure-Requests': 1
+
     }
 }
 
 function sleep(time = 0) {
-  return new Promise((resolve, reject) => {
-      setTimeout(() => {
-          resolve();
-      }, time);
-  });
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
 }
 
 //  获取章节列表 
-fetch(INDEX_URL, opt, function(error, meta, body) {
-  log(body.toString());
-  const $ = cheerio.load(body.toString());
+fetch(INDEX_URL, opt, function (error, meta, body) {
+    log(body.toString());
+    const $ = cheerio.load(body.toString());
 
-  const arr = [];
-  const h = $('.chapterlist li').each(function(i, elem) {
-    let url = $(elem).find('a').attr('href');
-    if(url)  {
-      arr.push(config.domain + url);
+    const arr = [];
+    const h = $('.chapterlist li').each(function (i, elem) {
+        let url = $(elem).find('a').attr('href');
+        if (url) {
+            arr.push(config.domain + url);
+        }
+    });
+
+    log(arr)
+
+    if (arr.length) {
+        setTimeout(() => {
+            batchForHtml(arr);
+        }, 200);
+    } else {
+        log('# 加载首页失败 #');
     }
-  });
-
-  log(arr)
-
-  if(arr.length) {
-    setTimeout(() => {
-      batchForHtml(arr);
-    }, 200);
-  } else {
-    log('# 加载首页失败 #');
-  }
 });
 
 async function batchForHtml(arr) {
-  const concurrency = config.concurrency;
-  const len = arr.length;
-  const listLen = Math.ceil(len /  concurrency);
-  // console.log('listLen:', listLen, len, concurrency);
-  const queuePromiseList = [];
-  for(let i = 0; i < concurrency; i++) {
-    const start = i * listLen;
-    const end = Math.min(start + listLen - 1, len - 1);
-    const tempArr = arr.slice(start, end + 1);
+    const concurrency = config.concurrency;
+    const len = arr.length;
+    const listLen = Math.ceil(len / concurrency);
+    // console.log('listLen:', listLen, len, concurrency);
+    const queuePromiseList = [];
+    for (let i = 0; i < concurrency; i++) {
+        const start = i * listLen;
+        const end = Math.min(start + listLen - 1, len - 1);
+        const tempArr = arr.slice(start, end + 1);
 
-    // log('index:', i,  'start:', start, 'end:', end, 'total:', len);
-    queuePromiseList.push(startQueue(tempArr));
-  }
-  Promise.all(queuePromiseList).then(res => {
-    console.log('all done!!!');
-    let list = [];
-    res.forEach(item => {
-      list = list.concat(item);
+        // log('index:', i,  'start:', start, 'end:', end, 'total:', len);
+        queuePromiseList.push(startQueue(tempArr));
+    }
+    Promise.all(queuePromiseList).then(res => {
+        console.log('all done!!!');
+        let list = [];
+        res.forEach(item => {
+            list = list.concat(item);
+        });
+        fs.writeFileSync(`./${config.bookName}.json`, JSON.stringify(list));
+
+        // 开始制作 -> 变为手动触发 gen.js
+        // require('./gen.js');
     });
-    fs.writeFileSync(`./${config.bookName}.json`, JSON.stringify(list));
-
-    // 开始制作 -> 变为手动触发 gen.js
-    // require('./gen.js');
-  });
 }
 
 function startQueue(arr) {
-  return new Promise((resolve, reject) => {
-    const pall = arr.map(url => {
-      return function() {
-        return fetchItem(url);
-      }
+    return new Promise((resolve, reject) => {
+        const pall = arr.map(url => {
+            return function () {
+                return fetchItem(url);
+            }
+        });
+
+        const contentArr = [];
+        const qp = new QueuePromise(pall, {
+            callback: function () {
+                console.log('===all done===');
+                resolve(contentArr);
+            },
+            errorInterrupt: false
+        });
+
+        let index = 0;
+
+        qp.on('success', res => {
+            console.log('success', ++index, res.title);
+            contentArr.push(res);
+        });
+        qp.run();
     });
-  
-    const contentArr = [];
-    const qp = new QueuePromise(pall, {
-      callback: function() {
-        console.log('===all done===');
-        resolve(contentArr);
-      },
-      errorInterrupt: false
-    });
-  
-    let index = 0;
-  
-    qp.on('success', res => {
-      console.log('success', ++index, res.title);
-      contentArr.push(res);
-    });
-    qp.run();
-  });
 }
 
 function fetchItem(url) {
-  // console.log(url)
+    // console.log(url)
 
-  return new Promise(function(reslove, reject) {
+    return new Promise(function (reslove, reject) {
 
-    setTimeout(() => {
-      fetch(url, opt, function(error, meta, body) {
-        
-        const $ = cheerio.load(body.toString());
-        
-        const html = $('#content').html();
-        const title  = $('.chaptertitle').text();
-        // console.log('title:', title);
-        if(!title) log(body.toString());
-        reslove({
-          html,
-          title
-        });
-      });
-    }, 100)
+        setTimeout(() => {
+            fetch(url, opt, function (error, meta, body) {
 
-    
-  }); 
+                const $ = cheerio.load(body.toString());
+
+                const html = $('#content').html();
+                const title = $('.chaptertitle').text();
+                // console.log('title:', title);
+                if (!title) log(body.toString());
+                reslove({
+                    html,
+                    title
+                });
+            });
+        }, 2000)
+
+
+    });
 }
